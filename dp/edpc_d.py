6411a828262d2d006ae7d2d6b50714bd4bbc68e0
yuizho <yuizho3@gmail.com>
2025-06-29 23:32:18 +0900

    modify dp codes

--- dp/edpc_d.py
+++ dp/edpc_d.py
@@ -2,11 +2,18 @@ N, W = map(int, input().split())
 # [(w, v)]
 items = [tuple(map(int, input().split())) for _ in range(N)]
 
-dp = [0] * (W + 1)
+dp = [[0] * (W + 1) for _ in range(N + 1)]
 
-for n in range(N):
-    w, v = items[n]
-    for i in range(W, w - 1, -1):
-        dp[i] = max(dp[i], dp[i - w] + v)
+for i in range(1, N + 1):
+    w, v = items[i - 1]
 
-print(max(dp))
+    for j in range(W + 1):
+        # とりあえず一つ上のitemのdp行を引き継ぎ
+        dp[i][j] = dp[i - 1][j]
+        # 一つまえの行からナップサックに入るだけ価値を埋めていくイメージ
+        # wが4ならでWが８ならj=4~8の間dp[i][j]に値が設定されていく
+        # これにより、過去に取得した価値との足し合わせも実装される
+        if j >= w:
+            dp[i][j] = max(dp[i][j], dp[i - 1][j - w] + v)
+
+print(max(dp[N]))
