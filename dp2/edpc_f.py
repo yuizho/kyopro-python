99787d2cc6c51334ad8889b088bc1a833f15dc6a
yuizho <yuizho3@gmail.com>
2025-08-02 16:09:20 +0900

    modify dp codes

--- dp2/edpc_f.py
+++ dp2/edpc_f.py
@@ -9,25 +9,22 @@ dp = [[0] * (len_t + 1) for _ in range(len_s + 1)]
 for i in range(1, len_s + 1):
     for j in range(1, len_t + 1):
         if s[i - 1] == t[j - 1]:
-            # 文字が一致する場合はdpのカウント更新
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
-            # 一致しない場合はdpテーブルの上 or 左のカウント数の大きい方を引き継ぎ
             dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
 
-    print(dp[i])
-
 lcs = ""
-i, j = len_s, len_t
+i = len_s
+j = len_t
 
-while i > 0 and j > 0:
+while 0 < i and 0 < j:
     if s[i - 1] == t[j - 1]:
         lcs = s[i - 1] + lcs
         i -= 1
         j -= 1
-    elif dp[i - 1][j] > dp[i][j - 1]:
-        i -= 1
-    else:
+    elif dp[i - 1][j] < dp[i][j - 1]:
         j -= 1
+    else:
+        i -= 1
 
 print(lcs)
