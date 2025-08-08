ec0d52a1d746c1798825c60a70bd3e67ca2e1bfd
yuizho <yuizho3@gmail.com>
2025-08-08 23:58:02 +0900

    add dp codes

---
+++ dp4/edpc_c.py
@@ -0,0 +1,11 @@
+N = int(input())
+activities = [tuple(map(int, input().split())) for _ in range(N)]
+
+dp = [[0, 0, 0] for _ in range(N + 1)]
+
+for i in range(1, N + 1):
+    dp[i][0] = max(dp[i - 1][1], dp[i - 1][2]) + activities[i - 1][0]
+    dp[i][1] = max(dp[i - 1][0], dp[i - 1][2]) + activities[i - 1][1]
+    dp[i][2] = max(dp[i - 1][0], dp[i - 1][1]) + activities[i - 1][2]
+
+print(max(dp[N]))
