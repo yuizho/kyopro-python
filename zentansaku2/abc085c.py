e658bfd879a2cc087b9c395c7abbfe8495d86eb7
yuizho <yuizho3@gmail.com>
2025-08-11 20:29:23 +0900

    modify dir name

---
+++ zentansaku2/abc085c.py
@@ -0,0 +1,10 @@
+N, Y = map(int, input().split())
+
+for i in range(N + 1):
+    for j in range(N - i + 1):
+        k = N - i - j
+        if 10000 * i + 5000 * j + 1000 * k == Y:
+            print(i, j, k)
+            exit()
+
+print(-1, -1, -1)
