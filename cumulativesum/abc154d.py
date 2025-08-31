7dc6be43ef789d4e4f0888349de2631ee084bf59
yuizho <yuizho3@gmail.com>
2025-08-31 13:54:54 +0900

    add cumulative sum codes

---
+++ cumulativesum/abc154d.py
@@ -0,0 +1,19 @@
+N, K = map(int, input().split())
+P = list(map(int, input().split()))
+
+cum_expected = [0.0] * (N + 1)
+for i in range(1, N + 1):
+    current_expected_value = sum(range(1, P[i - 1] + 1)) / P[i - 1]
+    cum_expected[i] = cum_expected[i - 1] + current_expected_value
+
+# print(cum_expected)
+
+left = 0
+right = K
+result = 0.0
+while right <= N:
+    result = max(result, cum_expected[right] - cum_expected[left])
+    left += 1
+    right += 1
+
+print(result)
